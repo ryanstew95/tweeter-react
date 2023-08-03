acb5d6817ca3de3f1873390e1880146171664e1b
Ryan Stewart <ryanstew17@gmail.com>
2023-08-03 12:13:30 -0300

    tweetform

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -1,10 +1,18 @@
 import React from 'react'; //optional
 
 const formMessage = "What are you humming about?"
+
+const handleSubmit = (event) => {
+
+  event.preventDefault();
+ 
+};
+
+
 function TweetForm(){
   return (
     <section className="newtweet">
-    <form method="post" action="/tweets" className="newtweet__form">
+    <form method="post" action="/tweets" className="newtweet__form" onSubmit={handleSubmit}>
       <textarea className="form__textarea" name="text" placeholder={formMessage}></textarea>
       <input type="submit" value="Tweet" className="form__input"/>
       <span className="form__counter">140</span>
